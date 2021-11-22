4f23895ad7f450b0302f3f2eda3344d88d9b139b
amirrasooli69 <amirrasooli69@gmail.com>
2021-11-22 05:06:42 +0330

    correct build qr code

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
+import React, {Component} from "react";
+import './App.css';
+import QRCode from "react-qr-code";
 
-import React, { Component } from 'react';
-import Navbar from './components/Navbar'
-import './index.css';
-import Banner from './components/Banner';
-import Cards from './components/Cards';
-import Search from './components/Search';
-import Logos from './components/Logos';
-import Footer from './components/Footer';
-import BuildCard from './components/BuildCard';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <BuildCard />
-        <Navbar />
-        <Banner />
-        <Cards />
-        <Search />
-        <Logos />
-        <Footer />
-      </div>
-    );
-  }
+
+class App extends Component {  
+        
+    // a = "BEGIN:VCARD\r\nFN:Amir Rasooli\r\nTITLE:Developer\r\nADR;Home:Modiran,Semnan,pelak,35\r\nADR;WORK:baghfamily,fari4,golfam;;;;;;\r\nTEL;HOME:02333445422\r\nTEL;WORK:02399887766\r\nTEL;MOBILE:09127173428\r\nURL;Site:www.papiloo.ir\r\nEMAIL;INTERNET,HOME:amirrasooli69@gmail.com\r\nEND:VCARD"
+   
+    qr = "BEGIN:VCARD\r\nFN:"
+        + this.props.name
+        + "\r\nTITLE:"
+        + this.props.title 
+        + "\r\nADR;Home:"
+        + this.props.addHome
+        + "\r\nADR;WORK:"
+        + this.props.addWrok
+        + "\r\nTEL;HOME:"
+        + this.props.telHome
+        + "\r\nTEL;WORK:"
+        + this.props.telWork
+        + "\r\nTEL;MOBILE:"
+        + this.props.mobile
+        + "\r\nURL;Site:"
+        + this.props.site
+        + "\r\nEMAIL;home:"
+        + this.props.emailPrivacy
+        + "\r\nEMAIL;work:"
+        + this.props.emailWork
+        + "\r\nEND:VCARD"
+
+
+    render () {
+        return(
+            <div >
+                {/* <div>{console.log(this.qr)}</div> */}
+                <QRCode value={ this.qr } size= '200' bgColor= "#F4F1BB" fgColor="#45404F" level= 'H' />
+
+            </div>
+        )
+    }
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 import App from './App';
 
-ReactDOM.render(<App />,document.getElementById('root'));
-
+ 
+ ReactDOM.render(<App name="Vahid Rasooli" emailPrivay="amirrasooli69@yahoo.com" addHome="Modiran,Semnan,pelak,35"
+ addWork="baghfamily,fari4,golfam;;;;;;" telHome="02333445422" telWork="02199887766"mobile="09127173428"
+  site="www.papiloo.ir" emailPrivacy="vahidrasooli75@yahoo.com" emailWork="vr7@gmail.com"/>, document.getElementById("root"));
